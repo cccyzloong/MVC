fa1e7948cb6930a6ad3c94faf17300891f9ccdb4
unknown <Beslic@.(none)>
2014-07-12 21:35:05 +0200

    - PHP MVC

--- app/controller/index.php
+++ app/controller/index.php
@@ -18,29 +18,5 @@
 			
 			$this->view->smarty->display('index/run.tpl');
 		}
-		
-		public function foo()
-		{
-			//pre_r(__METHOD__);
-
-			if($this->Post){
-				$this->model->insertData($this->Post);
-			}
-						
-			$this->view->smarty->assign('data', $this->model->getData());
-			
-			$this->view->smarty->display('index/foo.tpl');
-		}
-		
-		public function delete()
-		{
-			//pre_r(__METHOD__);
-			
-			if($this->Post){
-				$this->model->deleteData($this->Post);
-			}
-			
-			$this->redirect('index/foo');
-		}
 	}
 	
--- app/templates/index/run.tpl
+++ app/templates/index/run.tpl
@@ -1,17 +1,5 @@
 {extends file="../document.tpl"}
 
 {block name="body"}
-	We are in {$smarty.template}<br /><br />
-	
-	{if !empty($get)}
-		<pre>GET 
-			{print_r($get)}
-		</pre>
-	{/if}
-	
-	{if !empty($post)}
-		<pre>POST 
-			{print_r($post)}
-		</pre>
-	{/if}
+	We are in {$smarty.template}
 {/block}
