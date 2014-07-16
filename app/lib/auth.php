7e39bca2f8d48b0bca50c7c9b46e704f6de92230
unknown <davor.beslic@.mehrdafon.local>
2014-07-16 13:50:11 +0200

    - PHP MVC

--- app/lib/auth.php
+++ app/lib/auth.php
@@ -17,16 +17,17 @@
 				$this->_password = isset($args['password']) && !empty($args['password']) ? $args['password'] : FALSE;
 			}
 			
-			if($this->_username && $this->_password){			
-				//pre_r($this->_checkUser($this->_username, $this->_password));
+			if($this->_username && $this->_password){
+				$user = $this->_checkUser($this->_username, $this->_password);
+				$_SESSION['loged_user_id'] = $user['id'];
+				
+				pre_r($user);
 			}
 		}
 		
 		private function _checkUser($username, $password){
 			if($this->_username && $this->_password){
-				$stmt = $this->model->DB->prepare('SELECT * FROM user WHERE email = ? AND password = ?')->execute(array($this->_username, md5($this->_password))); 
-				
-				return $stmt->fetchAll(); 
+				return $this->model->DB->query('SELECT * FROM user WHERE email = "' . $this->_username . '" AND password = "' . md5($this->_password) . '"')->fetch(); 
 			}
 		}
 	}
--- app/templates/login/run.tpl
+++ app/templates/login/run.tpl
@@ -1,16 +1,14 @@
 {extends file="../document.tpl"}
 
 {block name="body"}	
-	<div class="col col-lg-3">
-		<form role="form" method="post">
+	<div class="col col-lg-2 col-lg-offset-5">
+		<form role="form" method="post" class="form-login">
 			<div class="form-group">
 		    	<input type="email" class="form-control" name="username" placeholder="Username (E-Mail)" />
-			</div>
-			<div class="form-group">
 		    	<input type="password" class="form-control" name="password" placeholder="Password" />
 			</div>
 			<div class="form-group">
-				<button type="submit" class="btn btn-success">Login</button>
+				<button type="submit" class="btn btn-success form-submit">Login</button>
 			</div>
 		</form>
 	</div>
