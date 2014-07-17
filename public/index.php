68857c440c5fdcfddfe880c5155fecf608ad31f3
unknown <Beslic@.(none)>
2014-07-17 21:52:46 +0200

    - PHP MVC

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-	require_once('../app/bootstrap.php');
+	require_once(__DIR__ . '/../app/bootstrap.php');
 	
 	$app = new Bootstrap(isset($_GET['url']) ? $_GET['url'] : FALSE);
 	$app->run();
