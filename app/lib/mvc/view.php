3ad7003e0dad58de06abfa9a439e91454fafdfd1
unknown <Beslic@.(none)>
2014-07-23 20:00:11 +0200

    - PHP MVC

--- app/lib/mvc/view.php
+++ app/lib/mvc/view.php
@@ -10,8 +10,8 @@
 			include_once __DIR__ . '/../Smarty/libs/Smarty.class.php';
 			
 			$this->smarty = new Smarty();
-			$this->smarty->template_dir = __DIR__ . '/../../templates/';
-			$this->smarty->compile_dir = __DIR__ . '/../../temp/templates_c/';
+			$this->smarty->template_dir = __DIR__ . '/../../view/';
+			$this->smarty->compile_dir = __DIR__ . '/../../temp/view_c/';
 			$this->smarty->config_dir = __DIR__ . '/../../temp/configs/';
 			$this->smarty->cache_dir = __DIR__ . '/../../temp/cache/';
 			
